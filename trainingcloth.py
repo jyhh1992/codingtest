65ea8354389848c6ed73447ee58b8fb2ae2a9dc6
jyhh1992 <jyhh1992@gmail.com>
2022-06-11 23:24:01 +0900

    3thrid

---
+++ trainingcloth.py
@@ -0,0 +1,14 @@
+def solution(n, lost, reserve):
+    u = [1] *(n+2)
+    for i in reserve:
+        u[i] += 1
+    for i in lost:
+        u[i] -= 1
+    for i in range(1, n+1):
+        if u[i-1] == 0 and u[i] ==2: 
+            # 앞사람에게 빌려주는지
+            u[i -1 :i + 1] = [1,1]
+        elif u[i] ==  2 and u[i+1] == 0:
+            # 뒷사람에게 빌려주는지
+            u[i:i+2] = [1, 1]
+    return len([x for x in u[1:-1] if x> 0])
